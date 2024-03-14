0ea68a174524f8708f9cfafd3b9480ddd3055b7b
=sangeetadhiman2003 <=sangeetadhiman2003@gmail.com>
2024-03-14 17:27:45 +0530

    Checkout controller, view file implemented for payment

---
+++ app/controllers/checkout_controller.rb
@@ -0,0 +1,40 @@
+class CheckoutController < ApplicationController
+  def create
+    if @cart.pluck(:currency).uniq.length > 1
+      redirect_to products_path, alert: "You can not select products with different currencies in one checkout"
+    else
+      customer = Stripe::Customer.create(
+        #name: current_user.name,
+        email: current_user.email,
+        description: "Customer id: #{current_user.id}",
+      )
+      @session = Stripe::Checkout::Session.create({
+        customer: customer,
+        payment_method_types: ['card'],
+        line_items: @cart.collect { |item| item.to_builder.attributes! },
+        allow_promotion_codes: true,
+        mode: 'payment',
+        success_url: success_url + "?session_id={CHECKOUT_SESSION_ID}",
+        cancel_url: cancel_url,
+      })
+      redirect_to @session.url
+    end
+  end
+
+  def success
+    if params[:session_id].present?
+      # session.delete(:cart)
+      session[:cart] = [] # empty cart = empty array
+      @session_with_expand = Stripe::Checkout::Session.retrieve({ id: params[:session_id], expand: ["line_items"]})
+      @session_with_expand.line_items.data.each do |line_item|
+        product = Product.find_by(stripe_product_id: line_item.price.product)
+      end
+    else
+      redirect_to cancel_url, alert: "No info to display"
+    end
+  end
+
+  def cancel
+  end
+
+end
